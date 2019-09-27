f40981f7fa380054e96baad48b8fab33011e07ca
DavenChan <396767101@qq.com>
2019-09-27 22:43:05 +0800

    .

--- src/main/java/demo/movieInteraction/controller/MovieInteractionTestController.java
+++ src/main/java/demo/movieInteraction/controller/MovieInteractionTestController.java
@@ -2,12 +2,15 @@ package demo.movieInteraction.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import httpHandel.HttpUtil;
 import movie.pojo.constant.MovieInteractionUrl;
+import movie.pojo.dto.FindMovieSummaryListDTO;
+import net.sf.json.JSONObject;
 
 @Controller
 @RequestMapping(value = "/mt", produces = "text/plain;charset=UTF-8")
@@ -16,12 +19,15 @@ public class MovieInteractionTestController {
 	@Autowired
 	private HttpUtil httpUtil;
 	
-	@GetMapping(value = "/test")
+	@PostMapping(value = "/test")
 	@ResponseBody
-	public String test() {
+	public String test(@RequestBody FindMovieSummaryListDTO dto) {
 		try {
+			dto.setPageParam();
+			JSONObject j = JSONObject.fromObject(dto);
+	        
 			String url = "http://127.0.0.1:10002" + MovieInteractionUrl.root + MovieInteractionUrl.simpleList;
-			String response = String.valueOf(httpUtil.sendGet(url));
+			String response = String.valueOf(httpUtil.sendPostRestful(url, j.toString()));
 			return response;
 		} catch (Exception e) {
 			e.printStackTrace();

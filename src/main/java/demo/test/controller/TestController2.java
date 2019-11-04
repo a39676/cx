1a37f2b5a6db638485d9ff3c84462c0a35a5b2a7
DavenChan <396767101@qq.com>
2019-11-05 00:02:04 +0800

    .

--- src/main/java/demo/test/controller/TestController2.java
+++ src/main/java/demo/test/controller/TestController2.java
@@ -2,15 +2,20 @@ package demo.test.controller;
 
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import demo.base.system.pojo.bo.IpRecordBO;
+import demo.base.user.pojo.po.UserIp;
 import demo.baseCommon.controller.CommonController;
 import demo.config.costom_component.SnowFlake;
 import demo.test.pojo.constant.TestUrl;
+import numericHandel.NumericUtilCustom;
 
 @Controller
 @RequestMapping(value = { TestUrl.testRoot2 })
@@ -21,6 +26,9 @@ public class TestController2 extends CommonController {
 
 	@Autowired
 	private SnowFlake snowFlake;
+	
+	@Autowired
+	private NumericUtilCustom numberUtil;
 
 	@GetMapping(value = "/snowFlake")
 	@ResponseBody
@@ -37,5 +45,19 @@ public class TestController2 extends CommonController {
 		redisTemplate.opsForSet().add("s1", "i3");
 		
 		return redisTemplate.opsForSet().members("s1");
+	}
+	
+	public void t(HttpServletRequest request) {
+		/*
+		 * TODO
+		 */
+		IpRecordBO record = getIp(request);
+		UserIp ui = new UserIp();
+		ui.setIp(numberUtil.ipToLong(record.getRemoteAddr()));
+		ui.setForwardIp(numberUtil.ipToLong(record.getForwardAddr()));
+		ui.setServerName(request.getServerName());
+		ui.setUri(request.getRequestURI());
+		ui.setUserId(2L);
+		
 	}
 }

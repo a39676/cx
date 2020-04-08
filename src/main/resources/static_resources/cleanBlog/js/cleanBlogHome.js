6412a0028bf4f7ee23382bfb6bd0e8938e981bb8
DavenChan <396767101@qq.com>
2020-04-08 17:55:32 +0800

    fix

--- src/main/resources/static_resources/cleanBlog/js/cleanBlogHome.js
+++ src/main/resources/static_resources/cleanBlog/js/cleanBlogHome.js
@@ -21,25 +21,6 @@ $(document).ready(function() {
     loadArticleLongSummaryFirstPage(uuid);
   });
 
-
-  document.getElementById("loginTag").onclick = function () {
-    showLogin()
-  };
-  
-  function showLogin(){ 
-    var url = "/login/login";
-    $.ajax({  
-      type : "GET",  
-      async : true,
-      url : url,  
-      success:function(datas){
-        $("#dynamicLoginDiv").html(datas);
-      },  
-      error: function(datas) {                
-      }  
-    });  
-  };
-
   var getLocation = function(href) {
       var location = document.createElement("a");
       location.href = href;

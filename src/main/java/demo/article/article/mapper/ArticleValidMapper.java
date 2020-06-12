094fc5a38867d79dd804d8cafafcc93b28c926c9
DavenChan <396767101@qq.com>
2020-06-12 14:01:52 +0800

    init

---
+++ src/main/java/demo/article/article/mapper/ArticleValidMapper.java
@@ -0,0 +1,30 @@
+package demo.article.article.mapper;
+
+import demo.article.article.pojo.po.ArticleValid;
+import demo.article.article.pojo.po.ArticleValidExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ArticleValidMapper {
+    long countByExample(ArticleValidExample example);
+
+    int deleteByExample(ArticleValidExample example);
+
+    int deleteByPrimaryKey(Long articleId);
+
+    int insert(ArticleValid record);
+
+    int insertSelective(ArticleValid record);
+
+    List<ArticleValid> selectByExample(ArticleValidExample example);
+
+    ArticleValid selectByPrimaryKey(Long articleId);
+
+    int updateByExampleSelective(@Param("record") ArticleValid record, @Param("example") ArticleValidExample example);
+
+    int updateByExample(@Param("record") ArticleValid record, @Param("example") ArticleValidExample example);
+
+    int updateByPrimaryKeySelective(ArticleValid record);
+
+    int updateByPrimaryKey(ArticleValid record);
+}

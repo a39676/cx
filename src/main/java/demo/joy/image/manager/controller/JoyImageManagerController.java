75b0d1ea4b3c5de5aae53b73ab6da9f6a67ab417
DavenChan <396767101@qq.com>
2020-08-29 13:54:10 +0800

    fix

--- src/main/java/demo/joy/image/manager/controller/JoyImageManagerController.java
+++ src/main/java/demo/joy/image/manager/controller/JoyImageManagerController.java
@@ -2,7 +2,7 @@ package demo.joy.image.manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,7 +18,7 @@ public class JoyImageManagerController {
 	@Autowired
 	private JoyImageManagerService imageManagerService;
 	
-	@PostMapping(value = JoyImageManagerUrl.CLEAN_JOY_IMAGE_REDIS)
+	@GetMapping(value = JoyImageManagerUrl.CLEAN_JOY_IMAGE_REDIS)
 	@ResponseBody
 	public JoyCommonResult cleanJoyImageRedis() {
 		return imageManagerService.cleanJoyImageRedis();

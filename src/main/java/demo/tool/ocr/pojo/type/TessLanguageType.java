285ac854c573bd5df13e41670cd85d666b008da9
DavenChan <396767101@qq.com>
2020-09-19 23:36:38 +0800

    memo update;
    update TessLanguageType;

--- src/main/java/demo/tool/ocr/pojo/type/TessLanguageType.java
+++ src/main/java/demo/tool/ocr/pojo/type/TessLanguageType.java
@@ -2,25 +2,40 @@ package demo.tool.ocr.pojo.type;
 
 public enum TessLanguageType {
 	
-	CHI_SIM("chi_sim"),
-	CHI_TRA("chi_tra"),
-	ENG("eng"),
+	ENG("eng", 0),
+	CHI_SIM("chi_sim", 1),
+	CHI_TRA("chi_tra", 2),
 	
 	;
 	
 	private String name;
+	private Integer code;
 	
-	TessLanguageType(String name) {
+	TessLanguageType(String name, Integer code) {
 		this.name = name;
+		this.code = code;
 	}
 	
 	public String getName() {
 		return name;
 	}
+	
+	public Integer getCode() {
+		return code;
+	}
 
-	public static TessLanguageType getType(String typeName) {
+	public static TessLanguageType getType(String name) {
 		for(TessLanguageType t : TessLanguageType.values()) {
-			if(t.getName().equals(typeName)) {
+			if(t.getName().equals(name)) {
+				return t;
+			}
+		}
+		return null;
+	}
+	
+	public static TessLanguageType getType(Integer code) {
+		for(TessLanguageType t : TessLanguageType.values()) {
+			if(t.getCode().equals(code)) {
 				return t;
 			}
 		}

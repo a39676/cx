d216f6c3f8fab8c7f2bd32e368b0212540396d0f
DavenChan <396767101@qq.com>
2020-10-03 15:38:59 +0800

    building joy skill service

---
+++ src/main/java/demo/joy/skill/service/impl/JoySkillBattleServiceImpl.java
@@ -0,0 +1,26 @@
+package demo.joy.skill.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import demo.joy.common.service.JoyCommonService;
+import demo.joy.skill.pojo.type.JoySkillType;
+import demo.joy.skill.service.JoySkillBattleService;
+
+@Service
+public class JoySkillBattleServiceImpl extends JoyCommonService implements JoySkillBattleService {
+
+	/*
+	 * TODO
+	 * 需要双方所有数据
+	 * 双方人物id, 双方buff Map,  
+	 */
+	public void skillHandler(JoySkillType skillType) {
+		if(JoySkillType.firstMiracle.equals(skillType)) {
+			firstMiracle();
+		}
+	}
+	
+	private void firstMiracle() {
+//		TODO
+	}
+}

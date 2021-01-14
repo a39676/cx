6ac6f519d4942e739d8c993fff502fd6f24c878c
DavenChan <396767101@qq.com>
2021-01-14 22:54:11 +0800

    add telegram test

--- src/main/java/demo/test/controller/TestController2.java
+++ src/main/java/demo/test/controller/TestController2.java
@@ -2,11 +2,15 @@ package demo.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import demo.common.controller.CommonController;
+import demo.finance.cryptoCoin.mq.producer.TelegramMessageAckProducer;
 import demo.test.pojo.constant.TestUrl;
 import demo.test.service.TestService;
+import telegram.pojo.dto.TelegramMessageDTO;
 
 @Controller
 @RequestMapping(value = { TestUrl.root2 })
@@ -16,4 +20,14 @@ public class TestController2 extends CommonController {
 	@Autowired
 	private TestService testService;
 
+	@Autowired
+	private TelegramMessageAckProducer telegramMessageAckProducer;
+	
+	@GetMapping(value = "/telTest")
+	public void telTest(@RequestParam(value = "msg") String msg, @RequestParam(value = "id") Long id) {
+		TelegramMessageDTO dto = new TelegramMessageDTO();
+		dto.setMsg(msg);
+		dto.setId(id);
+		telegramMessageAckProducer.send(dto);
+	}
 }

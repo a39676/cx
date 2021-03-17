ebc1e875c2100d1ba5f94df8693ca171f4ee596f
DavenChan <396767101@qq.com>
2021-03-18 00:56:52 +0800

    testing crypto coin compare

---
+++ src/main/java/demo/finance/cryptoCoin/tool/controller/CryptoCoinDataCompareContoller.java
@@ -0,0 +1,34 @@
+package demo.finance.cryptoCoin.tool.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import auxiliaryCommon.pojo.type.CurrencyType;
+import demo.common.controller.CommonController;
+import demo.finance.cryptoCoin.tool.pojo.constant.CryptoCoinDataCompareUrl;
+import demo.finance.cryptoCoin.tool.pojo.dto.CryptoCoinDataCompareDTO;
+import demo.finance.cryptoCoin.tool.pojo.result.CryptoDataCompareResult;
+import demo.finance.cryptoCoin.tool.service.CryptoDataCompareService;
+import finance.cryptoCoin.pojo.type.CryptoCoinType;
+
+@Controller
+@RequestMapping(value = CryptoCoinDataCompareUrl.ROOT)
+public class CryptoCoinDataCompareContoller extends CommonController {
+
+	@Autowired
+	private CryptoDataCompareService dataCompareService;
+	
+	@GetMapping(value = "/testing")
+	@ResponseBody
+	public CryptoDataCompareResult testing() {
+		CryptoCoinDataCompareDTO dto = new CryptoCoinDataCompareDTO();
+		dto.setCoinType1(CryptoCoinType.FIL.getCode());
+		dto.setCoinType2(CryptoCoinType.BTC.getCode());
+		dto.setCurrencyType(CurrencyType.USD.getCode());
+		dto.setStartDateTimeStr("2021-01-01 00:00:00");
+		return dataCompareService.cryptoCoinDailyDataComparePoint(dto);
+	}
+}
---
+++ src/main/java/demo/finance/cryptoCoin/tool/pojo/constant/CryptoCoinDataCompareUrl.java
@@ -0,0 +1,8 @@
+package demo.finance.cryptoCoin.tool.pojo.constant;
+
+import finance.cryptoCoin.pojo.constant.CryptoCoinPriceCommonUrl;
+
+public class CryptoCoinDataCompareUrl {
+
+	public static final String ROOT = CryptoCoinPriceCommonUrl.ROOT + "/dataCompare";
+}

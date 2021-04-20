b826921faea724fc9b30ee2b773b3a5bdd8cac30
DavenChan <396767101@qq.com>
2021-04-20 12:13:02 +0800

    fix

--- src/main/java/demo/finance/cryptoCoin/notice/pojo/constant/CryptoCoinNoticeConstant.java
+++ src/main/java/demo/finance/cryptoCoin/notice/pojo/constant/CryptoCoinNoticeConstant.java
@@ -3,6 +3,6 @@ package demo.finance.cryptoCoin.notice.pojo.constant;
 public class CryptoCoinNoticeConstant {
 
 	public static final String HIT_NO_DATA_COUNTING_KEY = "cryptoCoinNoticeHitNoDataCounting";
-	public static final int HIT_NO_DATA_COUNTING_LIMIT = 30;
+	public static final int HIT_NO_DATA_COUNTING_LIMIT = 20;
 	
 }

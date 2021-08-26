3593512fa21c81029c8deaa82bb5003fe0586add
DavenChan <q39676@gmail.com>
2021-08-26 16:20:53 +0800

    删除冗余

--- src/main/java/demo/finance/cryptoCoin/tool/service/CryptoCoinDataQueryService.java
+++
@@ -1,9 +0,0 @@
-package demo.finance.cryptoCoin.tool.service;
-
-import finance.cryptoCoin.pojo.dto.CryptoCoinDailyDataQueryDTO;
-
-public interface CryptoCoinDataQueryService {
-
-	void insertCryptoCoinDailyDataCollect(CryptoCoinDailyDataQueryDTO dto);
-
-}

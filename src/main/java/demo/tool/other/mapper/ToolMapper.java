7f2cddfe37b3a325c161f1914d26208bde38932c
DavenChan <q39676@gmail.com>
2021-09-07 20:28:40 +0800

    删除冗余

--- src/main/java/demo/tool/other/mapper/ToolMapper.java
+++ src/main/java/demo/tool/other/mapper/ToolMapper.java
@@ -1,8 +1,5 @@
 package demo.tool.other.mapper;
 
-import org.apache.ibatis.annotations.Param;
-
 public interface ToolMapper {
 
-	int complexInsert(@Param("tableName")String tableName, @Param("values")String values);
 }

951d5fab68db52ca84ff2a1e8d54b1e5d4ef00e9
DavenChan <q39676@gmail.com>
2021-11-12 07:47:24 +0800

    Update
    Add ComplexController

---
+++ src/main/java/demo/toyParts/complex/ComplexController.java
@@ -0,0 +1,15 @@
+package demo.toyParts.complex;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ComplexController {
+
+	@GetMapping(value = "/1jlbdmb")
+	@ResponseBody
+	public String for_1jlbdmb() {
+		return "pong";
+	}
+}

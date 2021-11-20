ee59affe9a02b62a30780e5bf45e97aa38ecbfc2
DavenChan <q39676@gmail.com>
2021-11-20 13:33:05 +0800

    Update

--- src/main/resources/static_resources/cleanBlog/js/articleNormal.js
+++ src/main/resources/static_resources/cleanBlog/js/articleNormal.js
@@ -10,32 +10,32 @@ var getUrlParameter = function getUrlParameter(sParam) {
       return sParameterName[1] === undefined ? true : sParameterName[1];
     }
   }
-};  
+};
 function loadArticleChannels() {
   var url = "/article/findChannels";
-  $.ajax({  
-    type : "POST",  
+  $.ajax({
+    type : "POST",
     async : true,
-    url : url, 
+    url : url,
     // data: JSON.stringify(jsonOutput),
     cache : false,
     dataType: 'json',
     contentType: "application/json;",
     beforeSend: function(xhr) {
       xhr.setRequestHeader(csrfHeader, csrfToken);
-    }, 
+    },
     success:function(datas){
       datas.channelList.forEach(function(channel) {
         $("#articleChannels").append(buildSubChannel(channel));
-      });  
-      var channelId = datas.channelList[0].channelId;  
-      setTimeout(function(){}, 800);  
+      });
+      var channelId = datas.channelList[0].channelId;
+      setTimeout(function(){}, 800);
       loadArticleLongSummaryFirstPage(channelId);
-    },  
+    },
     error: function(datas) {
-    }  
-  });  
-}  
+    }
+  });
+}
 function buildSubChannel(channel) {
   var channelButton = "";
   channelButton += "<button class='btn btn-sm channelButton'";
@@ -45,13 +45,13 @@ function buildSubChannel(channel) {
   channelButton += channel.channelName;
   channelButton += "</button>";
   return channelButton;
-}  
+}
 function buildSummaryLine(subArticleVO) {
   var newRow = " ";
   newRow += " <div class='post-preview'> ";
   newRow += " <a href='/article/readArticleLong?pk="+subArticleVO.privateKey+"' target='_blank'> ";
   newRow += " <h2 class='post-title'>";
-  if (subArticleVO.isHot) { 
+  if (subArticleVO.isHot) {
     newRow += " <span style='text-decoration-line: underline; color: rgb(255, 156, 0); background-color: rgb(255, 255, 255);'>"+subArticleVO.articleTitle+"</span> ";
   } else {
     newRow += subArticleVO.articleTitle;
@@ -68,7 +68,7 @@ function buildSummaryLine(subArticleVO) {
   newRow += " </div> ";
   newRow += " <hr >"
   return newRow;
-}  
+}
 function loadArticleLongSummary(channelId, withHot) {
   var blogArea = $("#blogArea");
   blogArea.attr("articleChannel", channelId);
@@ -76,12 +76,12 @@ function loadArticleLongSummary(channelId, withHot) {
   if(blogArea.attr("loadingFlag") == "1") {
     return;
   }
-  $("#articleAreaLoadingImg").fadeIn(100);    
+  $("#articleAreaLoadingImg").fadeIn(100);
   blogArea.attr("loadingFlag", "1");
   var jsonOutput = {
     articleChannelId:channelId,
     isHot:withHot,
-  };  
+  };
   if(withHot == "true") {
     jsonOutput['vcode'] = getUrlParameter('vcode');
   } else {
@@ -89,14 +89,14 @@ function loadArticleLongSummary(channelId, withHot) {
   }
   var url = "/article/articleLongSummaryListByChannel";
   $.ajax({
-    type : "POST",  
+    type : "POST",
     async : true,
-    url : url,  
+    url : url,
     data: JSON.stringify(jsonOutput),
     cache : false,
     contentType: "application/json",
     dataType: "json",
-    timeout:50000,  
+    timeout:50000,
     beforeSend: function(xhr) {
       xhr.setRequestHeader(csrfHeader, csrfToken);
     },
@@ -110,19 +110,20 @@ function loadArticleLongSummary(channelId, withHot) {
         blogRowArea.append(newRow);
         blogArea.attr("markTime", subArticleVO.createDateTimeString);
       });
-    },  
-    error: function(datas) {  
+    },
+    error: function(datas) {
+      location.reload();
     }
-  }); 
+  });
   $("#articleAreaLoadingImg").fadeOut(100);
   setTimeout(function(){
     blogArea.attr("loadingFlag", "0");
   }, 500);
-};  
+};
 function loadArticleLongSummaryFirstPage(channelId) {
   $("#blogArea").attr("markTime", "");
   $(".channelButton").prop('disabled', true);
-  $("#searchByTitle").prop('disabled', true);  
+  $("#searchByTitle").prop('disabled', true);
   var blogRowArea = $("#blogRowArea");
   blogRowArea.html("");
   setTimeout(function(){
@@ -131,8 +132,8 @@ function loadArticleLongSummaryFirstPage(channelId) {
       $(".channelButton").prop('disabled', false);
       $("#searchByTitle").prop('disabled', false);
     }, 1000);
-  }, 500);      
-}  
+  }, 500);
+}
 $("#loadMoreButton").click(function () {
   var channelId = $("#blogArea").attr("articleChannel");
   if(channelId != null && channelId.length > 0) {
@@ -140,53 +141,53 @@ $("#loadMoreButton").click(function () {
   } else {
     searchArticleLongSummary(false);
   }
-});  
+});
 $("#createNewArticle").click(function () {
   var win = window.open("/article/creatingArticleLong", '_blank');
   win.focus();
-});  
+});
 $("#searchByTitle").click(function () {
   searchArticleLongSummary(true);
-});  
+});
 function searchArticleLongSummary(firstSearchFlag) {
   $(".channelButton").prop('disabled', true);
-  $("#searchByTitle").prop('disabled', true);    
+  $("#searchByTitle").prop('disabled', true);
   var newKeyword = $("#searchKeyWord").val();
   var blogArea = $("#blogArea");
   var blogRowArea = $("#blogRowArea");
-  var channelId = blogArea.attr("articleChannel");  
+  var channelId = blogArea.attr("articleChannel");
   if (firstSearchFlag) {
     blogArea.attr("articleChannel", "");
     blogArea.attr("markTime", "");
     blogRowArea.html("");
   } else {
-  }  
+  }
   var markTime = blogArea.attr("markTime");
-  var oldKeyword = blogArea.attr("keyword");  
+  var oldKeyword = blogArea.attr("keyword");
   if(blogArea.attr("loadingFlag") == "1") {
     return;
   }
-  $("#articleAreaLoadingImg").fadeIn(100);    
-  blogArea.attr("loadingFlag", "1");  
+  $("#articleAreaLoadingImg").fadeIn(100);
+  blogArea.attr("loadingFlag", "1");
   var jsonOutput = {
     endTime:markTime,
     isHot:"false",
-  };  
+  };
   if (firstSearchFlag) {
     jsonOutput["title"] = newKeyword;
   } else {
     jsonOutput["title"] = oldKeyword;
-  }      
+  }
   var url = "/article/articleLongSummaryListByChannel";
   $.ajax({
-    type : "POST",  
+    type : "POST",
     async : true,
-    url : url,  
+    url : url,
     data: JSON.stringify(jsonOutput),
     cache : false,
     contentType: "application/json",
     dataType: "json",
-    timeout:50000,  
+    timeout:50000,
     beforeSend: function(xhr) {
       xhr.setRequestHeader(csrfHeader, csrfToken);
     },
@@ -200,13 +201,13 @@ function searchArticleLongSummary(firstSearchFlag) {
         blogRowArea.append(newRow);
         blogArea.attr("markTime", subArticleVO.createDateTimeString);
       });
-    },  
-    error: function(datas) {  
+    },
+    error: function(datas) {
     }
-  }); 
+  });
   $("#articleAreaLoadingImg").fadeOut(100);
   setTimeout(function(){}, 800);
-  blogArea.attr("loadingFlag", "0");    
+  blogArea.attr("loadingFlag", "0");
   setTimeout(function(){
     $(".channelButton").prop('disabled', false);
     $("#searchByTitle").prop('disabled', false);

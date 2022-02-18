7a2a3cf1301020e8e5fc989550332580103c7292
DavenChan <q39676@gmail.com>
2022-02-18 15:22:55 +0800

    Update
    Clean test mapper

--- src/main/java/demo/test/mapper/TestMapper.java
+++ src/main/java/demo/test/mapper/TestMapper.java
@@ -1,20 +1,9 @@
 package demo.test.mapper;
 
-import java.util.HashMap;
-import java.util.List;
-
-import demo.finance.cryptoCoin.data.pojo.dto.FindFirstReasonableDayDTO;
-import demo.finance.cryptoCoin.data.pojo.po.CryptoCoinPrice1day;
-
 public interface TestMapper {
 	
 	Object test1();
 	
 	Object throwExpection();
 	
-	List<CryptoCoinPrice1day> findCryptoCoinStartTime(FindFirstReasonableDayDTO dto);
-	
-	List<HashMap<Object, Object>> findDuplicateDailyData();
-	
-	List<HashMap<Object, Object>> findDuplicateDailyDataInTheSameDay();
 }

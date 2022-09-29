0cd6ff53c45778419e45cd943551c6090144fb12
DavenChan <demo@demo.com>
2022-09-29 12:32:39 +0800

    Debug

--- src/main/java/demo/pmemo/service/impl/UrgeNoticeServiceImpl.java
+++ src/main/java/demo/pmemo/service/impl/UrgeNoticeServiceImpl.java
@@ -46,7 +46,8 @@ public class UrgeNoticeServiceImpl extends ArticleCommonService implements UrgeN
 	@Override
 	public void setUpdateMsgWebhook(String secretToken) {
 		String hostname = hostnameService.findMainHostname();
-		String webhookUrl = "https://" + hostname + UrgeNoticeUrl.ROOT + "/" + UrgeNoticeUrl.RECEIVE_URGE_NOTICE_MSG;
+		String webhookUrl = "https://" + hostname + UrgeNoticeUrl.ROOT + UrgeNoticeUrl.RECEIVE_URGE_NOTICE_MSG;
+		log.error("Set web hook: " + webhookUrl);
 		telegramService.setWebhook(TelegramBotType.URGE_NOTICE.getName(), webhookUrl, secretToken);
 	}
 

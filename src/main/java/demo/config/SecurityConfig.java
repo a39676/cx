f0af5b987f31c8837280b858c7653ba4da98ecaa
DavenChan <demo@demo.com>
2023-02-25 18:19:50 +0800

    Update
    Follow ImageInteractionUrl.java update

--- src/main/java/demo/config/SecurityConfig.java
+++ src/main/java/demo/config/SecurityConfig.java
@@ -169,7 +169,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 	    web.ignoring()
 	    .antMatchers("/test/testIgnoring")
-	    .antMatchers(ImageInteractionUrl.root + "/**")
+	    .antMatchers(ImageInteractionUrl.ROOT + "/**")
 	    ;
 	}
 	

d859ca473a5d89fc61446611ed4b63082fa05b82
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-04-11 16:37:57 +0800

    Fix

--- src/main/java/demo/test/controller/TestController2.java
+++ src/main/java/demo/test/controller/TestController2.java
@@ -10,12 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import demo.common.service.CommonService;
-import demo.interaction.wechat.mq.producer.SendBonusRechargeTemplateMessageProducer;
-import demo.interaction.wechat.service.WechatUserService;
 import demo.test.pojo.constant.TestUrl;
 import demo.test.pojo.dto.TestDTO;
 import demo.test.service.TestService2;
-import wechatSdk.pojo.dto.SendTemplateMessageBonusRechargeDTO;
 
 @Controller
 @RequestMapping(value = { TestUrl.root2 })
@@ -35,28 +32,10 @@ public class TestController2 extends CommonService {
 		return "{\"k\":\"v\"}";
 	}
 
-	@Autowired
-	private SendBonusRechargeTemplateMessageProducer sender;
-
 	@GetMapping(value = "/t3")
 	@ResponseBody
 	public String t3() {
-		SendTemplateMessageBonusRechargeDTO dto = new SendTemplateMessageBonusRechargeDTO();
-		dto.setBonusAmountStr("bas");
-		dto.setBonusDescription("bd");
-		dto.setManagerCode("LqHvN5DTjlqMHtt2Jg539wDma7MlvopMnjmCp6j5g8T");
-		dto.setReciverOpenId("oXt5d5l_w5Fjl7IyaQ1SF7fTjS1g");
-		sender.send(dto);
 		return "Done";
 	}
 
-	@Autowired
-	private WechatUserService wechatUserService;
-
-	@GetMapping(value = "/t4")
-	@ResponseBody
-	public String t4() {
-		wechatUserService.bonusForNewPositiveUserInYesterday();
-		return "Done";
-	}
 }

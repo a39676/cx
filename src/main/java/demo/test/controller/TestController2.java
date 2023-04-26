ab1c19d0d5b956dd28b7fe157e600be65cf32872
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-04-26 12:40:50 +0800

    Update

--- src/main/java/demo/test/controller/TestController2.java
+++ src/main/java/demo/test/controller/TestController2.java
@@ -6,25 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import ai.aiArt.pojo.dto.TextToImageFromWechatDTO;
-import ai.aiArt.pojo.result.AiArtImageWallResult;
-import ai.aiArt.pojo.result.GetJobResultList;
-import ai.aiArt.pojo.result.SendTextToImgJobResult;
-import ai.aiArt.pojo.type.AiArtSamplerType;
-import demo.ai.aiArt.controller.AiArtApiController;
-import demo.ai.aiArt.pojo.dto.TextToImageFromApiDTO;
-import demo.ai.aiArt.service.AiArtService;
-import demo.ai.aiChat.service.AiChatUserService;
-import demo.common.pojo.dto.BaseDTO;
 import demo.common.service.ToolCommonService;
 import demo.test.pojo.constant.TestUrl;
 import demo.test.pojo.dto.TestDTO;
 import demo.test.service.TestService2;
-import toolPack.ioHandle.FileUtilCustom;
 
 @Controller
 @RequestMapping(value = { TestUrl.root2 })
@@ -50,74 +38,4 @@ public class TestController2 extends ToolCommonService {
 		return "Done";
 	}
 
-	@Autowired
-	private AiArtService aiArtService;
-	@Autowired
-	private AiChatUserService aiChatUserService;
-
-	@GetMapping(value = "/t4")
-	@ResponseBody
-	public String t4() {
-		FileUtilCustom f = new FileUtilCustom();
-		String content = f.getStringFromFile("d:/tmp/tmp.json");
-
-		TextToImageFromWechatDTO dto = buildObjFromJsonCustomization(content, TextToImageFromWechatDTO.class);
-		dto.setTmpKey(String.valueOf(getTmpKey()));
-		aiArtService.sendTextToImgFromWechatDtoToMq(dto);
-		return "Done";
-	}
-
-	private Long getTmpKey() {
-		Long tmpKey = aiChatUserService.createNewTmpKey(1L, "oXt5d5l_w5Fjl7IyaQ1SF7fTjS1g");
-		return tmpKey;
-	}
-
-	@GetMapping(value = "/t5")
-	@ResponseBody
-	public GetJobResultList t5() {
-		return aiArtService.getJobResultListByTmpKey(String.valueOf(getTmpKey()));
-	}
-
-	@Autowired
-	private AiArtApiController aiArtApiController;
-
-	@GetMapping(value = "/t6")
-	@ResponseBody
-	public SendTextToImgJobResult t6(@RequestParam("apiKey") String apiKey) {
-		TextToImageFromApiDTO dto = new TextToImageFromApiDTO();
-		dto.setApiKey(apiKey);
-		dto.setBatchSize(1);
-		dto.setWidth(12);
-		dto.setHeight(12);
-		dto.setCfgScale(7);
-		dto.setPrompts("p");
-		dto.setNegativePrompts("np");
-		dto.setSampler(AiArtSamplerType.DPM2_a_Karras.getName());
-		dto.setSteps(30);
-		return aiArtApiController.sendTextToImgFromApiDtoToMq(dto);
-	}
-
-	@GetMapping(value = "/t7")
-	@ResponseBody
-	public GetJobResultList t7(@RequestParam("pk") String pk) {
-		BaseDTO dto = new BaseDTO();
-		if ("".equals(pk)) {
-			pk = "C+d1kuoC+MtByBGS9y6hV1ShWfmjWxcr/ATw3Isr2CY=";
-		}
-		dto.setPk(pk);
-		return aiArtService.getJobResultVoByJobPk(dto);
-	}
-
-	@GetMapping(value = "/t8")
-	@ResponseBody
-	public AiArtImageWallResult t8() {
-		aiArtService.refreshImageWallJsonFile();
-		return aiArtService.getImageWall();
-	}
-
-	@GetMapping(value = "/t9")
-	@ResponseBody
-	public AiArtImageWallResult t9() {
-		return aiArtService.getImageWall(true);
-	}
 }

eec8fc113864539323fede4dd056a19469d6caa9
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-05-12 17:32:41 +0800

    Update
    Add AI art API

--- src/main/java/demo/ai/aiArt/controller/AiArtApiController.java
+++ src/main/java/demo/ai/aiArt/controller/AiArtApiController.java
@@ -2,6 +2,7 @@ package demo.ai.aiArt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import ai.aiArt.pojo.result.GetJobResultList;
 import ai.aiArt.pojo.result.SendTextToImgJobResult;
 import auxiliaryCommon.pojo.dto.BasePkDTO;
 import demo.ai.aiArt.pojo.dto.TextToImageFromApiDTO;
+import demo.ai.aiArt.pojo.result.GetAiArtAllModelListResult;
+import demo.ai.aiArt.pojo.result.GetAiArtAllSamplerResult;
+import demo.ai.aiArt.pojo.result.GetAiArtAllUpscalerResult;
 import demo.ai.aiArt.service.AiArtService;
 
 @Controller
@@ -26,12 +30,28 @@ public class AiArtApiController {
 	public SendTextToImgJobResult sendTextToImgFromApiDtoToMq(@RequestBody TextToImageFromApiDTO dto) {
 		return aiArtService.sendTextToImgFromApiDtoToMq(dto);
 	}
-	
+
 	@PostMapping(value = AiArtApiUrlConstant.GET_JOB_RESULT_BY_JOB_PK)
 	@ResponseBody
 	public GetJobResultList getJobResultVoByJobPk(@RequestBody BasePkDTO dto) {
 		return aiArtService.getJobResultVoByJobPk(dto);
 	}
+
+	@GetMapping(value=AiArtApiUrlConstant.GET_MODEL_LIST)
+	@ResponseBody
+	public GetAiArtAllModelListResult getModelList() {
+		return aiArtService.getAllModelList();
+	}
 	
+	@GetMapping(value=AiArtApiUrlConstant.GET_SAMPLER_LIST)
+	@ResponseBody
+	public GetAiArtAllSamplerResult getAllSamplerList() {
+		return aiArtService.getAllSamplerList();
+	}
 	
+	@GetMapping(value=AiArtApiUrlConstant.GET_UPSCALER_LIST)
+	@ResponseBody
+	public GetAiArtAllUpscalerResult getAllUpsalerList() {
+		return aiArtService.getAllUpsalerList();
+	}
 }

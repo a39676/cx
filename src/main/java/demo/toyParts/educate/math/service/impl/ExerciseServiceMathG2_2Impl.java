26cc8540702e0c2e118e444c57aefb2f6c7ceb2f
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-11-14 17:40:42 +0800

    Update

--- src/main/java/demo/toyParts/educate/math/service/impl/ExerciseServiceMathG2_2Impl.java
+++ src/main/java/demo/toyParts/educate/math/service/impl/ExerciseServiceMathG2_2Impl.java
@@ -136,6 +136,11 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 		exerciseDTO.getQuestionList().add(question);
 		questionNumber++;
 
+		question = createWordProblemModule15();
+		question.setQuestionNumber(questionNumber);
+		exerciseDTO.getQuestionList().add(question);
+		questionNumber++;
+
 		return exerciseDTO;
 	}
 
@@ -419,12 +424,12 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 		String[] keyWord = dynamicKeyWord.get(randomKeyWordIndex);
 
 		int item2Price = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
-		int times = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
-		int item1Price = item2Price * times;
+		int multiplication = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
+		int item1Price = item2Price * multiplication;
 
 //		String moduleStr = "一%s%s, 可以买%d%s%s, 如果每%s%s%d元, (1)每%s%s几元? (2)一%s%s比一%s%s便宜多少元?";
 		q.setExpression(String.format(moduleStr, keyWord[1], keyWord[0], // 一%s%s
-				times, keyWord[3], keyWord[2], // 可以买%d%s%s,
+				multiplication, keyWord[3], keyWord[2], // 可以买%d%s%s,
 				keyWord[1], keyWord[0], item1Price, // 如果每%s%s%d元
 				keyWord[3], keyWord[2], // (1)每%s%s几元?
 				keyWord[3], keyWord[2], keyWord[1], keyWord[0] // (2)一%s%s比一%s%s便宜多少元?";
@@ -460,9 +465,9 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 		int item2base = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
 		int item3base = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
 
-		int times1 = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
-		int times2 = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
-		int times3 = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
+		int multiplication1 = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
+		int multiplication2 = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
+		int multiplication3 = t.nextInt(MIN_DIVISION_NUM, MAX_DIVISION_NUM + 1);
 
 		int random1Add = 0;
 		if (item1base == MIN_DIVISION_NUM) {
@@ -485,9 +490,9 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 			random3Add = t.nextInt(MIN_DIVISION_NUM, item3base);
 		}
 
-		int item1Total = item1base * times1 + random1Add;
-		int item2Total = item2base * times2 + random2Add;
-		int item3Total = item3base * times3 + random3Add;
+		int item1Total = item1base * multiplication1 + random1Add;
+		int item2Total = item2base * multiplication2 + random2Add;
+		int item3Total = item3base * multiplication3 + random3Add;
 
 //		String moduleStr = "商店有%s%d%s, %s%d%s, %s%d%s, %s想用%d%s%s, %d%s%s, %d%s%s, 做成%s%s, 这些%s最多可以做成多少个这样的%s%s";
 //		dynamicKeyWord.add(new String[] { "条", "绿色丝带", "白色丝带", "彩色丝带", "丝带", "组合", "李叔叔" });
@@ -499,12 +504,12 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 				keyWord[4], keyWord[4], keyWord[5] // 这些%s最多可以做成多少个这样的%s%s"
 		));
 
-		int result = times1;
-		if (times2 < result) {
-			result = times2;
+		int result = multiplication1;
+		if (multiplication2 < result) {
+			result = multiplication2;
 		}
-		if (times3 < result) {
-			result = times3;
+		if (multiplication3 < result) {
+			result = multiplication3;
 		}
 
 		q.addStandardAnswer(String.valueOf(result));
@@ -667,8 +672,8 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 		String[] keyWords = dynamicKeyWord.get(randomKeyWordIndex);
 
 		int priceOfObjB = t.nextInt(12, 24);
-		int times = t.nextInt(3, 12);
-		int priceOfObjA = priceOfObjB * times;
+		int multiplication = t.nextInt(3, 12);
+		int priceOfObjA = priceOfObjB * multiplication;
 
 		int countingOfObjA = t.nextInt(3, 8);
 		int countingOfObjB = t.nextInt(8, 15);
@@ -681,7 +686,7 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 
 //		为了丰富同学们的课外生活, %s老师买了%d%s%s和%d%s%s, 共付了%d元, 按价钱算, 一%s%s可以换%d%s%s, 那么一%s%s需要多少钱呢?;
 		q.setExpression(String.format(moduleStr, keyWords[0], countingOfObjA, unit, objAName, countingOfObjB, unit,
-				objBName, totalAmount, unit, objAName, times, unit, objBName, unit, objBName));
+				objBName, totalAmount, unit, objAName, multiplication, unit, objBName, unit, objBName));
 
 		q.addStandardAnswer(String.valueOf(priceOfObjB));
 
@@ -876,6 +881,70 @@ public class ExerciseServiceMathG2_2Impl extends ExerciseMathCommonService imple
 		q.setExpression(String.format(moduleStr, intA, intB, intC));
 
 		q.addStandardAnswer(String.valueOf(lowestCommonMultiple));
+
+		return q;
+	}
+
+	private MathQuestionBaseDTO createWordProblemModule15() {
+		ThreadLocalRandom t = ThreadLocalRandom.current();
+		String moduleStr = "%s, %s, %s 共有%d%s,<br> %s, %s, %s, %s的%s數則剛好相等.<br> 問%s, %s, %s各有多少%s?";
+		List<String[]> dynamicKeyWord = new ArrayList<>();
+		dynamicKeyWord.add(new String[] { "小明", "小王", "小紅", "元", "錢" });
+		dynamicKeyWord.add(new String[] { "小紅", "小綠", "小藍", "顆", "糖" });
+		dynamicKeyWord.add(new String[] { "小狗", "小猴", "小兔", "條", "香蕉" });
+		dynamicKeyWord.add(new String[] { "小強", "小桃", "小葉", "張", "貼紙" });
+
+		String[] keyWords = dynamicKeyWord.get(t.nextInt(0, dynamicKeyWord.size()));
+		String roleA = keyWords[0];
+		String roleB = keyWords[1];
+		String roleC = keyWords[2];
+		String util = keyWords[3];
+		String objName = keyWords[4];
+
+		MathQuestionBaseDTO q = new MathQuestionBaseDTO();
+		q.setMathQuestionType(MathQuestionType.WORD_PROBLEM);
+
+		int total = 3;
+		int randomMultiplication = t.nextInt(6, 12);
+		total = total * randomMultiplication;
+		int intA = randomMultiplication;
+		int intB = randomMultiplication;
+		int intC = randomMultiplication;
+
+		int movingAB = t.nextInt(0 - randomMultiplication, randomMultiplication);
+		while (movingAB == 0) {
+			movingAB = t.nextInt(0 - randomMultiplication, randomMultiplication);
+		}
+		intA = intA - movingAB;
+		intB = intB + movingAB;
+		int movingBC = t.nextInt(0 - randomMultiplication, randomMultiplication);
+		while (movingBC == 0) {
+			movingBC = t.nextInt(0 - intB, intB);
+		}
+		intB = intB - movingBC;
+		intC = intC + movingBC;
+
+		String description1 = "";
+		if (movingAB < 0) {
+			description1 = "如果 " + roleA + " 給 " + roleB + " " + (0 - movingAB) + util + ", ";
+		} else {
+			description1 = "如果 " + roleB + " 給 " + roleA + " " + movingAB + util + ", ";
+		}
+
+		String description2 = "";
+		if (movingBC < 0) {
+			description2 = "然後 " + roleB + " 給 " + roleC + " " + (0 - movingBC) + util + ", ";
+		} else {
+			description2 = "然後 " + roleC + " 給 " + roleB + " " + movingBC + util + ", ";
+		}
+
+//		%s, %s, %s 共有%d%s, 
+//		%s, %s, %s, %s的%s數則剛好相等, 
+//		問%s, %s, %s各有多少%s?
+		q.setExpression(String.format(moduleStr, roleA, roleB, roleC, total, (util + objName),
+				(description1 + description2), roleA, roleB, roleC, objName, roleA, roleB, roleC, objName));
+
+		q.setStandardAnswer(Arrays.asList(String.valueOf(intA), String.valueOf(intB), String.valueOf(intC)));
 
 		return q;
 	}

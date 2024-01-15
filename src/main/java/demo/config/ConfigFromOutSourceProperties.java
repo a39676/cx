69d222dbab337a5d65b24c1eb4a36db86934dc6a
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-01-15 13:25:03 +0800

    Update

--- src/main/java/demo/config/ConfigFromOutSourceProperties.java
+++ src/main/java/demo/config/ConfigFromOutSourceProperties.java
@@ -11,19 +11,11 @@ public class ConfigFromOutSourceProperties {
 	private static final String MAIN_FOLDER_PATH = "/home/u2/cx/optionFile";
 
 	@Bean
-	public PropertySourcesPlaceholderConfigurer getDatabaseConfig() {
+	public PropertySourcesPlaceholderConfigurer getSystemConfig() {
 		PropertySourcesPlaceholderConfigurer properties = new PropertySourcesPlaceholderConfigurer();
 
-		properties.setLocation(new FileSystemResource(MAIN_FOLDER_PATH + "/databaseConfig.properties"));
-		properties.setIgnoreResourceNotFound(false);
-		return properties;
-	}
-
-	@Bean
-	public PropertySourcesPlaceholderConfigurer getRabbitMqConfig() {
-		PropertySourcesPlaceholderConfigurer properties = new PropertySourcesPlaceholderConfigurer();
-		properties.setLocation(new FileSystemResource(MAIN_FOLDER_PATH + "/rabbitMqConfig.properties"));
-		properties.setIgnoreResourceNotFound(false);
+		properties.setLocation(new FileSystemResource(MAIN_FOLDER_PATH + "/systemConfig.properties"));
+		properties.setIgnoreResourceNotFound(true);
 		return properties;
 	}
 

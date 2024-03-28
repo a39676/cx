f54a05573ac78e19c317b73c972baf224c1485b2
Daven <19781485+a39676@users.noreply.github.com>
2024-03-28 11:32:16 +0800

    Update
    Stop crypto coin notice task service

--- src/main/java/demo/finance/cryptoCoin/notice/service/impl/CryptoCoinNoticeTaskService.java
+++ src/main/java/demo/finance/cryptoCoin/notice/service/impl/CryptoCoinNoticeTaskService.java
@@ -1,26 +1,26 @@
-package demo.finance.cryptoCoin.notice.service.impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import demo.base.task.service.CommonTaskService;
-import demo.finance.cryptoCoin.notice.service.CryptoCoinCommonNoticeService;
-
-@Component
-public class CryptoCoinNoticeTaskService extends CommonTaskService {
-
-	@Autowired
-	private CryptoCoinCommonNoticeService noticeService;
-
-	@Scheduled(fixedDelay = 1000L * 30)
-	public void cryptoCoinPriceNoticeHandler() {
-		noticeService.noticeHandler();
-	}
-
-	@Scheduled(cron = "40 51 22 * * *")
-	public void deleteOldNotice() {
-		noticeService.deleteOldNotice();
-	}
-
-}
+//package demo.finance.cryptoCoin.notice.service.impl;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import demo.base.task.service.CommonTaskService;
+//import demo.finance.cryptoCoin.notice.service.CryptoCoinCommonNoticeService;
+//
+//@Component
+//public class CryptoCoinNoticeTaskService extends CommonTaskService {
+//
+//	@Autowired
+//	private CryptoCoinCommonNoticeService noticeService;
+//
+//	@Scheduled(fixedDelay = 1000L * 30)
+//	public void cryptoCoinPriceNoticeHandler() {
+//		noticeService.noticeHandler();
+//	}
+//
+//	@Scheduled(cron = "40 51 22 * * *")
+//	public void deleteOldNotice() {
+//		noticeService.deleteOldNotice();
+//	}
+//
+//}

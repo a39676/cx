e2d7fe2cbc1e7e27cc6916837b3608937f5349e3
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-08-15 09:31:52 +0800

    Merge branch 'dev_tool' of https://github.com/a39676/cx.git into dev_tool

--- src/main/java/demo/tool/other/service/impl/ComplexToolServiceImpl.java
+++ src/main/java/demo/tool/other/service/impl/ComplexToolServiceImpl.java
@@ -22,7 +22,7 @@ public class ComplexToolServiceImpl extends ToolCommonService implements Complex
 
 	@Override
 	public void notificationServiceDown(HeartBeatType heartBeatType) {
-		telegramService.sendMessageByChatRecordId(TelegramBotType.NORMAL_MSG, heartBeatType.getName() + " down",
+		telegramService.sendMessageByChatRecordId(TelegramBotType.CX_MESSAGE, heartBeatType.getName() + " down",
 				TelegramStaticChatID.MY_ID);
 	}
 

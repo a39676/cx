5f7f1408b2af6dd66626dd320a961d32459b2371
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-11-02 19:53:46 +0800

    .

--- src/main/java/demo/interaction/bbt/service/impl/BbtTaskService.java
+++ src/main/java/demo/interaction/bbt/service/impl/BbtTaskService.java
@@ -1,19 +1,15 @@
 package demo.interaction.bbt.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import demo.interaction.bbt.service.BbtComplexService;
 
 @Component
 public class BbtTaskService {
 
-	@Autowired
-	private BbtComplexService bbtComplexService;
-
-	@Scheduled(cron = "0 * * * * ?")
-	public void checkBbtIsAlive() {
-		bbtComplexService.checkBbtIsAlive();
-	}
+//	@Autowired
+//	private BbtComplexService bbtComplexService;
+//
+//	@Scheduled(cron = "0 * * * * ?")
+//	public void checkBbtIsAlive() {
+//		bbtComplexService.checkBbtIsAlive();
+//	}
 }

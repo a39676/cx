ad963345ce591806557b337d687048db564e2dda
Daven <19781485+a39676@users.noreply.github.com>
2026-09-25 11:48:43 +0800

    .

--- src/main/java/demo/tool/taobao/service/impl/TaobaoOfferRecordServiceImpl.java
+++ src/main/java/demo/tool/taobao/service/impl/TaobaoOfferRecordServiceImpl.java
@@ -295,7 +295,8 @@ public class TaobaoOfferRecordServiceImpl extends CommonService implements Taoba
 		}
 		TaobaoOfferFromDownstreamBuyerExample buyerOrderExample = new TaobaoOfferFromDownstreamBuyerExample();
 		Criteria criteria = buyerOrderExample.createCriteria();
-		criteria.andCreateTimeGreaterThanOrEqualTo(startTime).andCreateTimeLessThan(endTime).andIsDeleteEqualTo(false);
+		criteria.andOrderPaymentTimeGreaterThanOrEqualTo(startTime).andOrderPaymentTimeLessThan(endTime)
+				.andIsDeleteEqualTo(false);
 		if (StringUtils.isNotBlank(dto.getBuyerOrderId())) {
 			try {
 				Long buyerOrderId = Long.parseLong(dto.getBuyerOrderId());
